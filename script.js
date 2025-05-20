4f5f91a11838eca96ee53f4a6ba50a64f96bed47
Amiir25 <amirsadik636@gmail.com>
2025-05-20 17:56:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,29 @@ document.querySelectorAll('.short-nav-link').forEach((shortLink) => {
         document.querySelector('#navbar-small').classList.remove('-translate-y-0');
     })
 })
+
+// Projects
+const renderProjects = () => {
+    projects.forEach(project => {
+        // Project image
+        document.querySelector('#project-image').setAttribute('src', `${project.image}`);
+        // Project title
+        document.querySelector('#project-title').textContent = project.title;
+        // Project description
+        document.querySelector('#project-description').textContent = project.description;
+
+        // Loop throught tech stacks
+        for (let i = 0; i < project.tech.length; i++) {
+            document.querySelector('#tech-stack').innerHTML += `
+                <img src="${project.tech[i]}" alt="${project.title}" class="w-[25px]">
+            `
+        }
+        alert(document.querySelector('#tech-stack').innerHTML);
+
+
+        // Github Link
+        document.querySelector('#github-link').setAttribute('href', `${project.github} `);
+        document.querySelector('#live-link').setAttribute('href', `${project.live} `);
+    })
+}
+renderProjects();
