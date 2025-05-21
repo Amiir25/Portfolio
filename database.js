0534a4ad1f4c6b0889086023313c8cb8ab68f587
Amiir25 <amirsadik636@gmail.com>
2025-05-21 17:18:55 +0300

    Rename projects.js to database.js

---
+++ database.js
@@ -0,0 +1,109 @@
+// Skills
+const skills = [
+    "./img/tools/html.webp",
+    "./img/tools/css.webp",
+    "./img/tools/javascript.webp",
+    "./img/tools/tailwind-css.png",
+    "./img/tools/bootstrap.webp",
+    "./img/tools/c.webp",
+    "./img/tools/python.webp",
+    "./img/tools/git.webp",
+    "./img/tools/chat-gpt.webp"
+];
+
+// Projects
+const projects = [
+    // {
+    //     "id": 1,
+    //     "image": "./img/projects/portfolio-img.PNG",
+    //     "title": "Portfolio",
+    //     "description": "A portfolio website showcasing my passion and skills on application development.",
+    //     "tools": [
+    //         "./img/tools/html.webp",
+    //         "./img/tools/tailwind-css.png"
+    //     ],
+    //     "github": "https://github.com/Amiir25/Portfolio.git",
+    //     "demo": "https://amiir25.github.io/Portfolio/"
+    // },
+    {
+        "id": 2,
+        "desktopimage": "./img/projects/app-works-desktop.webp",
+        "mobileimage": "./img/projects/app-works-mobile.webp",
+        "title": "AppWorks",
+        "description": "AppWorks is a tech company offering various technology solutions for businesses and companies. This is a single landing page for AppWorks, featuring hero messaging, service overview, client tetimonials and more.",
+        "tools": [
+            "./img/tools/html.webp",
+            "./img/tools/css.webp",
+            "./img/tools/javascript.webp"
+        ],
+        "github": "https://github.com/Amiir25/AppWorks.git",
+        "demo": "https://amiir25.github.io/AppWorks/"
+    },
+    {
+        "id": 3,
+        "desktopimage": "./img/projects/eventER-desktop.webp",
+        "mobileimage": "./img/projects/eventER-mobile.webp",
+        "title": "eventER",
+        "description": "A dynamic, fully responsive event booking site allowing users to book from a hetrogeneous list of events. Event details and a strong form validation features are included.",
+        "tools": [
+            "./img/tools/css.webp",
+            "./img/tools/tailwind-css.png",
+            "./img/tools/javascript.webp"
+        ],
+        "github": "https://github.com/Amiir25/eventER.git",
+        "demo": "https://amiir25.github.io/eventER/"
+    },
+    // {
+    //     "id": 4,
+    //     "image": "./img/projects/netflix-img.PNG",
+    //     "title": "Netflix Clone",
+    //     "description": "A responsive clone of the Netflix home page.",
+    //     "tools": [
+    //         "./img/tools/html.webp",
+    //         "./img/tools/css.webp",
+    //         "./img/tools/javascript.webp"
+    //     ],
+    //     "github": "https://github.com/Amiir25/Netflix_Clone.git",
+    //     "demo": "https://amiir25.github.io/Netflix_Clone/"
+    // },
+];
+
+// Contacts
+const contacts = [
+    {
+        "id": 1,
+        "name": "Email",
+        "image": "./img/contacts/contact-gmail.webp",
+        "link": "mailto:amirsadik636@gmail.com"
+    },
+    {
+        "id": 2,
+        "name": "LinkedIn",
+        "image": "./img/contacts/contact-linkedin.webp",
+        "link": "linkedin.com/in/amir-sadik-20b2a9291"
+    },
+    {
+        "id": 3,
+        "name": "Telegram",
+        "image": "./img/contacts/contact-telegram.webp",
+        "link": "https://t.me/amir_sadik"
+    },
+    {
+        "id": 4,
+        "name": "Whatsapp",
+        "image": "./img/contacts/contact-whatsapp.webp",
+        "link": "https://web.whatsapp.com/"
+    },
+    {
+        "id": 5,
+        "name": "X",
+        "image": "./img/contacts/contact-x.webp",
+        "link": "https://x.com/0x0A_mir"
+    },
+    {
+        "id": 6,
+        "name": "GitHub",
+        "image": "./img/contacts/contact-github.webp",
+        "link": "https://github.com/Amiir25"
+    }
+];
