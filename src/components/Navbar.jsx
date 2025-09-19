c4e31ae731685fb822f32a595bd56d388038eb53
Amiir25 <amirsadik636@gmail.com>
2025-09-19 11:52:56 +0300

    Add light/dark mode icons on the navbar in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun } from '@fortawesome/free-regular-svg-icons';
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = ({ darkMode, setDarkMode }) => {
   return (
@@ -6,13 +9,20 @@ const Navbar = ({ darkMode, setDarkMode }) => {
         {/* Logo */}
         <img src="/A-logo.png" alt="" className='w-20 md:w-30' />
 
-        {/* Links */}
-        <div className='flex gap-4 md:text-xl'>
-            <a href="#" className='hover:text-indigo-200 active:text-white'>About</a>
-            <a href="#" className='hover:text-indigo-200 active:text-white'>Skills</a>
-            <a href="#" className='hover:text-indigo-200 active:text-white'>Education</a>
-            <a href="#" className='hover:text-indigo-200 active:text-white'>Projects</a>
-            <a href="#" className='hover:text-indigo-200 active:text-white'>Contact</a>
+        <div className='flex items-center gap-8'>
+            {/* Dark/light mode icon */}
+            <FontAwesomeIcon icon={ darkMode ? faSun : faMoon }
+            onClick={ () => setDarkMode(!darkMode) }
+            className='md:text-xl cursor-pointer' />
+
+            {/* Nav links */}
+            <div className='flex gap-4 md:text-xl'>
+                <a href="#" className='hover:text-indigo-200 active:text-white'>About</a>
+                <a href="#" className='hover:text-indigo-200 active:text-white'>Skills</a>
+                <a href="#" className='hover:text-indigo-200 active:text-white'>Education</a>
+                <a href="#" className='hover:text-indigo-200 active:text-white'>Projects</a>
+                <a href="#" className='hover:text-indigo-200 active:text-white'>Contact</a>
+            </div>
         </div>
     </div>
   )
