c3ed21f39050b2e70d053e20da2f9db8387c96ee
Amiir25 <amirsadik636@gmail.com>
2025-09-19 16:53:37 +0300

    Minimize the size of logo image and navbar links in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun } from '@fortawesome/free-regular-svg-icons';
-import { faMoon } from '@fortawesome/free-solid-svg-icons'
+// import { } from '@fortawesome/free-regular-svg-icons';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = ({ darkMode, setDarkMode }) => {
   return (
-    <div className='flex items-center justify-between'>
+    <div className='flex items-center justify-between py-2'>
         {/* Logo */}
-        <img src="/A-logo.png" alt="" className='w-20 md:w-30' />
+        <img src="/A-logo.png" alt="" className='w-20 md:w-28' />
 
         <div className='flex items-center gap-8'>
             {/* Dark/light mode icon */}
@@ -16,7 +16,7 @@ const Navbar = ({ darkMode, setDarkMode }) => {
             className='md:text-xl cursor-pointer' />
 
             {/* Nav links */}
-            <div className='flex gap-4 md:text-xl'>
+            <div className='flex gap-4 md:text-lg'>
                 <a href="#" className='hover:text-indigo-200 active:text-white'>About</a>
                 <a href="#" className='hover:text-indigo-200 active:text-white'>Skills</a>
                 <a href="#" className='hover:text-indigo-200 active:text-white'>Education</a>
