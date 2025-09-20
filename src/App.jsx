d406a0ca7b2423a623a6154b02db8dba7671d435
Amiir25 <amirsadik636@gmail.com>
2025-09-20 16:19:09 +0300

    Add Contacts component and footer in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import Header from './components/Header';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Contacts from './components/Contacts';
 
 const App = () => {
 
   const [darkMode, setDarkMode] = useState(true);
 
   return (
-    <div className={`relative px-6 md:px-12 lg:px-24 xl:px-32
+    <div className={`relative px-12 md:px-24 lg:px-48 xl:px-60
     ${ darkMode ? 'bg-slate-950 text-gray-50' : 'bg-gray-50 text-slate-950' } transition-all duration-300`}>
 
       <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
       <About darkMode={darkMode}/>
       <Skills darkMode={darkMode} />
       <Projects darkMode={darkMode} />
+      <Contacts darkMode={darkMode} />
+
+      <p className='mt-20 text-center border-t border-slate-500 py-2'>
+        &copy; 2024 - 2025. <a href="">Amir Sadik</a>
+      </p>
 
     </div>
   )
