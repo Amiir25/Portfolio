efd0d1acca0f486fd94c5140d9ba13f0267e5246
Amiir25 <amirsadik636@gmail.com>
2025-09-20 10:01:09 +0300

    Add Skills component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Header from './components/Header';
 import About from './components/About';
+import Skills from './components/Skills';
 
 const App = () => {
 
@@ -8,10 +9,11 @@ const App = () => {
 
   return (
     <div className={`relative px-6 md:px-12 lg:px-24 xl:px-32
-    ${ darkMode ? 'bg-slate-900 text-gray-50' : 'bg-gray-50 text-slate-900' } transition-all duration-300`}>
+    ${ darkMode ? 'bg-slate-950 text-gray-50' : 'bg-gray-50 text-slate-950' } transition-all duration-300`}>
 
       <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
       <About darkMode={darkMode}/>
+      <Skills darkMode={darkMode} />
 
     </div>
   )
