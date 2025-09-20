61626f24544f1ad1c9907245ea9c0c8713d27423
Amiir25 <amirsadik636@gmail.com>
2025-09-20 10:03:08 +0300

    Add Skills section in Skills.jsx

---
+++ src/components/Skills.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { skills } from '../assets/assets'
+
+const Skills = ({ darkMode }) => {
+  return (
+    <div className='mt-40 pb-10'>
+      <h1 className='text-3xl sm:text-4xl md:text-6xl text-indigo-400 text-center font-bold mb-10 md:mb-20'>Skills & Technologies</h1>
+      <div className='flex flex-wrap gap-4 items-center justify-center'>
+        {
+          skills.map((skill, i) => (
+            <div key={i} className={`flex flex-col gap-1 items-center w-30 md:w-40 h-24 md:h-30 p-8
+            ${ darkMode ? 'text-slate-50 bg-slate-800 border-slate-500 shadow-indigo-100/30' : 
+            'text-slate-950 bg-slate-200 border-slate-400 shadow-indigo-950'} border rounded
+            hover:shadow-2xl transition-all duration-300`}>
+              <img src={ skill.image } alt={`${skill.name} Image`} className='w-8 md:w-12' />
+              <p className='text-xs font-medium'>{ skill.name }</p>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Skills
