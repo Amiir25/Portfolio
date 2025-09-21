38d8cf8892ceac6face7ed6b157cdc9e34ebb62d
Amiir25 <amirsadik636@gmail.com>
2025-09-21 08:02:41 +0300

    Add a background image on the header section in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,10 @@ import Hero from './Hero'
 
 const Header = ({ darkMode, setDarkMode }) => {
   return (
-    <div style={{ backgroundImage: 'url(/bg-main-2.png)' }} className='bg-cover bg-bottom bg-fixed'>
-      <div className='h-screen flex flex-col items-center justify-center px-12 md:px-24 lg:px-48 xl:px-60
-      bg-slate-950/75'>
+    <div style={{ backgroundImage: darkMode ? 'url(/bg-main-dark.png)' : 'url(/bg-main-light.png)' }}
+    className='bg-cover bg-center bg-fixed bg-no-repeat'>
+      <div className={`h-screen flex flex-col items-center justify-center px-12 md:px-24 lg:px-48 xl:px-60
+      ${ darkMode ? 'bg-slate-950/60' : 'bg-slate-50/60' }`}>
         <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
         <Hero darkMode={darkMode} />
       </div>
