42b9003ae13d221e169fa575e9c8178899d95bda
Amiir25 <amirsadik636@gmail.com>
2025-09-21 08:09:56 +0300

    Update the contrast of the background image in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({ darkMode, setDarkMode }) => {
     <div style={{ backgroundImage: darkMode ? 'url(/bg-main-dark.png)' : 'url(/bg-main-light.png)' }}
     className='bg-cover bg-center bg-fixed bg-no-repeat'>
       <div className={`h-screen flex flex-col items-center justify-center px-12 md:px-24 lg:px-48 xl:px-60
-      ${ darkMode ? 'bg-slate-950/60' : 'bg-slate-50/60' }`}>
+      ${ darkMode ? 'bg-slate-950/40' : 'bg-slate-200/40' }`}>
         <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
         <Hero darkMode={darkMode} />
       </div>
