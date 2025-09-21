13636296d181a6f04d359184ecbe4ec67e92dfb9
Amiir25 <amirsadik636@gmail.com>
2025-09-21 08:24:48 +0300

    Chnage the bacjground images to webp in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Hero from './Hero'
 
 const Header = ({ darkMode, setDarkMode }) => {
   return (
-    <div style={{ backgroundImage: darkMode ? 'url(/bg-main-dark.png)' : 'url(/bg-main-light.png)' }}
+    <div style={{ backgroundImage: darkMode ? 'url(/bg-main-dark.webp)' : 'url(/bg-main-light.webp)' }}
     className='bg-cover bg-center bg-fixed bg-no-repeat'>
       <div className={`h-screen flex flex-col items-center justify-center px-12 md:px-24 lg:px-48 xl:px-60
       ${ darkMode ? 'bg-slate-950/40' : 'bg-slate-200/40' }`}>
